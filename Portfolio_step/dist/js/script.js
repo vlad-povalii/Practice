b016e6bbad361903de755b3d42c78a72f4a971ce
Vladyslav <vp@gmail.com>
2023-10-29 16:48:11 +0100

    final project step

---
+++ Portfolio_step/dist/js/script.js
@@ -0,0 +1,23 @@
+// menu section - close and open + overlay
+const hamburgerEl = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const closeEl = document.querySelector('.menu__close');
+const overlayEl = document.querySelector('.menu__overlay');
+
+hamburgerEl.addEventListener('click', () => {
+    menu.classList.add('active');
+})
+closeEl.addEventListener('click', () => {
+    menu.classList.remove('active');
+})
+overlayEl.addEventListener('click', () => {
+    menu.classList.remove('active');
+})
+
+// skills section - rating-line
+const counters = document.querySelectorAll('.skills__rating-counter');
+const lines = document.querySelectorAll('.skills__rating-line span');
+
+counters.forEach((item, index) => {
+    lines[index].style.width = item.innerText;
+})
